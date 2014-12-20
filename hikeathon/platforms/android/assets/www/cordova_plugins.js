0c39a073c166fde7f736a09d6d09e9b4d5eb452c
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-12-20 15:53:08 +0530

    fuck all

--- hikeathon/platforms/android/assets/www/cordova_plugins.js
+++ hikeathon/platforms/android/assets/www/cordova_plugins.js
@@ -13,6 +13,20 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -20,7 +34,8 @@ module.exports.metadata =
 {
     "com.ionic.keyboard": "1.0.3",
     "org.apache.cordova.console": "0.2.12",
-    "org.apache.cordova.device": "0.2.13"
+    "org.apache.cordova.device": "0.2.13",
+    "org.apache.cordova.device-motion": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
