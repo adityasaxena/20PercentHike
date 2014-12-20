0ede25637a1519c0680206ff3f85f2a122121bcd
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2014-12-20 18:37:23 +0530

    adding init and login functionalities for facebook

--- hikeathon/www/js/app.js
+++ hikeathon/www/js/app.js
@@ -1,11 +1,12 @@
 // Ionic Starter App
+//Facebook App ID: 311894645668189
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaFacebook) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +16,13 @@ angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    console.log($cordovaFacebook.init(311894645668189));
+
+    $cordovaFacebook.login(['user_about_me' , 'user_activities', 'user_birthday'], function(result) {
+      console.log(JSON.stringify(result));
+    }, function(error) {
+      console.log(JSON.stringify(error));
+    });
   });
-})
+});
