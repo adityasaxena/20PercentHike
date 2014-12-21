c2ff030fa8ffa35a5be612ed60c43df97583fb87
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2014-12-21 09:19:17 +0530

    constantly getting user movements.

--- hikeathon/www/js/controllers/userstate.js
+++ hikeathon/www/js/controllers/userstate.js
@@ -1,4 +1,17 @@
 angular.module('starter')
-    .controller('UserStateCtrl', function($scope, $state, userstate) {
-
-    });
+.controller('UserStateCtrl', function ($scope, $state, userstate) {
+	var watch = userstate.watch().then(
+		function (success) {
+			//nothing here
+		},
+		function (error) {
+			//nothign here
+		},
+		function (accl) {
+			var X = accl.x;
+			var Y = accl.y;
+			var Z = accl.z;
+			$scope.userstate = "X: " + X + "    Y:" + Y + "       Z:" + Z;
+			var timeStamp = accl.timestamp;
+		});
+});
--- hikeathon/www/js/services/userstate.js
+++ hikeathon/www/js/services/userstate.js
@@ -1,5 +1,28 @@
-// Sapy
 angular.module('starter')
-    .service('userstate', function($rootScope, $cordovaDeviceMotion) {
+.service('userstate', function ($rootScope, $cordovaDeviceMotion) {
+	var states = [];
 
-    });
+	var options = {
+		maximumAge: 1000,
+		timeout: 60 * 1000,
+		enableHighAccuracy: false // may cause errors if true
+	};
+
+	var watch = function() {
+		return $cordovaDeviceMotion.watchAcceleration(options);
+	};
+
+	var addState = function(x, y, z) {
+		states.push([x, y, z]);
+	};
+
+	var clearWatch = function(id) {
+		$cordovaDeviceMotion.clearWatch(id);
+	};
+
+	return {
+		addState: addState,
+		watch: watch,
+		clearWatch: clearWatch
+	}
+});
