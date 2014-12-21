95266b8adeab5823a166a2abb50b503c790b78fc
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-12-21 12:06:42 +0530

    wifi interpret logic

--- hikeathon/www/js/services/orientation.js
+++ hikeathon/www/js/services/orientation.js
@@ -1,7 +1,11 @@
 // Sapy
 angular.module('starter')
     .service('orientation', function($rootScope) {
-        this.showOrientation = function(argument) {
+        this.showOrientation = function() {
+            if (!navigator.compass) {
+                $rootScope.orientattionError = 'No orientation sensor found';
+                return false;
+            }
             var watchID = navigator.compass.getCurrentHeading(function onSuccess(heading) {
                 $rootScope.orientattion = heading.magneticHeading;
                 // alert($rootScope.orientattion);
--- hikeathon/www/js/services/wifi.js
+++ hikeathon/www/js/services/wifi.js
@@ -2,23 +2,28 @@
 angular.module('starter')
     .service('wifi', function($rootScope, $localStorage) {
 
-        $rootScope.wifiWatch = {};
+        var lastWifidetectedAt;
+        var pollingTime = 10000;
 
         var updateLocalStorage = function(data) {
-            // bssid chnage detected
-            if ($rootScope.bssid !== data.BSSID) {
+            // Wifi is used for atleast 10 seconds
+            if (new Date() - lastWifidetectedAt > pollingTime) {
                 // Check If this bssid is previously used bssid
                 if ($localStorage.wifi !== undefined) {
-                    var existingbssids = _.pluck($localStorage.wifi, 'bssid');
+                    var existingWifi = _.find($localStorage.wifi, {
+                        bssid: data.BSSID
+                    });
 
                     // If this BSSID is previously used
-                    if (existingbssids.indexOf(data.BSSID) > -1) {
-
+                    if (existingWifi) {
+                        existingWifi.usedForSeconds = existingWifi.usedForSeconds + (new Date() - lastWifidetectedAt);
+                        existingWifi.lastTime = new Date();
                     } else {
                         $localStorage.wifi.push({
                             bssid: data.BSSID,
                             ssid: data.SSID,
-                            timeline: new Date()
+                            lastTime: new Date(),
+                            usedForSeconds: pollingTime
                         });
                     }
                 } else {
@@ -27,9 +32,12 @@ angular.module('starter')
                     $localStorage.wifi.push({
                         bssid: data.BSSID,
                         ssid: data.SSID,
-                        timeline: newDate()
+                        lastTime: new Date(),
+                        usedForSeconds: 0
                     });
                 }
+
+                lastWifidetectedAt = new Date();
             }
 
         }
@@ -44,6 +52,9 @@ angular.module('starter')
             updateLocalStorage({
                 'BSSID': 'test:id'
             });
+            if (!lastWifidetectedAt) {
+                lastWifidetectedAt = new Date();
+            }
             if (navigator.wifi) {
                 navigator.wifi.getAccessPoints(function(data) {
                     updateLocalStorage(data[0]);
