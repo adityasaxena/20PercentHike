1728c2da1abc1580861ca3e87d62de2c2d910fc4
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-12-21 19:05:07 +0530

    user action logic

--- hikeathon/www/js/services/accelometer.js
+++ hikeathon/www/js/services/accelometer.js
@@ -1,36 +1,103 @@
 // Sapy
 angular.module('starter')
-    .service('accelometer', function($rootScope) {
-        var options = {
-            frequency: 3000
-        }; // Update every 3 seconds
+    .service('accelometer', function($rootScope, $localStorage) {
+        var lastAccSavedAt;
+        var pollTime = 5000;
+
+        $rootScope.userAction = $localStorage.userAction || {
+            'Last stationary At': '',
+            'Last walked At': '',
+            'Last Ran At': '',
+            'Last Drove At': '',
+            'Last Flew At': ''
+        };
+
+
+        var max = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
 
         var updateRootScope = function(acceleration) {
             $rootScope.AccelerationX = acceleration.x;
             $rootScope.AccelerationY = acceleration.y;
             $rootScope.AccelerationZ = acceleration.z;
-            $rootScope.Accelerationtimestamp = acceleration.timestamp;
-        }
 
-        var watchAccelometerChange = function() {
-            var watchID = navigator.accelerometer.watchAcceleration(function onSuccess(acceleration) {
-                updateRootScope(acceleration);
-            }, function(error) {
-                $rootScope.accelerationError = JSON.stringify(error);
-            }, options);
-        }
+            var maxOfCurrent = Math.max(acceleration.x, acceleration.y, acceleration.z);
+            var currentTime = new Date().toString().substr(4, 20);
+
+            if (maxOfCurrent > 200) {
+                $rootScope.userAction['Last Flew At'] = currentTime;
+            } else if (maxOfCurrent > 28 && maxOfCurrent < 200) {
+                $rootScope.userAction['Last Drove At'] = currentTime;
+            } else if (maxOfCurrent > 16 && maxOfCurrent <= 28) {
+                $rootScope.userAction['Last Ran At'] = currentTime;
+            } else if (maxOfCurrent > 10 && maxOfCurrent <= 16) {
+                $rootScope.userAction['Last walked At'] = currentTime;
+            } else if (maxOfCurrent > 9 && maxOfCurrent <= 10) {
+                $rootScope.userAction['Last stationary At'] = currentTime;
+            }
+
+            var maxOfMax = Math.max(max.x, max.y, max.z);
+            if (maxOfCurrent > maxOfMax) {
+                max.y = acceleration.y;
+                max.x = acceleration.x;
+                max.z = acceleration.z;
+            }
+            processAccelometer(max.x, max.y, max.z);
+        };
+
+        var processAccelometer = function(x, y, z) {
+
+            if (new Date() - lastAccSavedAt > pollTime) {
+                debugger
+                lastAccSavedAt = new Date();
+                var prevMax = Math.max($localStorage.maxacc.x, $localStorage.maxacc.y, $localStorage.maxacc.z);
+                var maxOfMax = Math.max(x, y, z);
+
+                if (maxOfMax > prevMax) {
+                    $localStorage.maxacc.x = x;
+                    $localStorage.maxacc.y = y;
+                    $localStorage.maxacc.z = z;
+                    $localStorage.maxacc.maxOn = new Date();
+                }
+                $localStorage.userAction = $rootScope.userAction;
+                $rootScope.maxacc = $localStorage.maxacc;
+            }
+        };
 
         this.tryAcclometer = function() {
+            if (!lastAccSavedAt) {
+                lastAccSavedAt = new Date();
+                if ($localStorage.maxacc === undefined) {
+                    $localStorage.maxacc = {
+                        x: 0,
+                        y: 0,
+                        z: 0,
+                        maxOn: new Date()
+                    };
+                }
+            }
             var acc = navigator.accelerometer;
+
+            // Testing code change x,y,z to see changes
+
+            // updateRootScope({
+            //     x: 1,
+            //     y: 17,
+            //     z: 4,
+            //     timestamp: new Date()
+            // });
+
+            // Test code ends
+
             if (acc) {
                 acc.getCurrentAcceleration(function(acceleration) {
                     updateRootScope(acceleration);
                 }, function(error) {
                     $rootScope.accelerationError = JSON.stringify(error);
                 });
-
-                // also constantly watch accelometer changes.
-                // watchAccelometerChange();
             } else {
                 $rootScope.accelerationError = 'Sorry No accelometer found';
             }
